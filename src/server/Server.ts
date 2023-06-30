bb96131f76e38a85ae8b513def7a92f2d6352299
Saullo Reis <saulloreis01@hotmail.com>
2023-06-30 11:56:52 -0300

    feat: adicionando o cors no server

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { router } from '../routes';
+import cors from 'cors'
+import { main } from '../database';
 
 const server = express()
 
+server.use(cors({
+    origin: 'http://127.0.0.1:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 server.use(express.json())
+main()
 server.use(router)
 
 export {server};
